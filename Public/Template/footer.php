9728fd827f5099235513bd0b5b3b73796c32345e
YC <lyc950421@gmail.com>
2018-08-22 14:09:36 +0800

    Updated footer template

--- Public/Template/footer.php
+++ Public/Template/footer.php
@@ -1,5 +1,9 @@
 <footer class="footer">
     <div class="container">
-    @ 2018
+    <div class="row">
+        <div class="col-md-12">
+        @ 2018
+        </div>
+    </div>
     </div>
 </footer>
--- register.php
+++ register.php
@@ -40,7 +40,7 @@
             $result=mysqli_query($conn,$query);
             ?>
 
-    <div class="container flex">
+    <div class="container">
             <form action="register.php" method="POST">
                 <div class="form-group">
                     <label for="Name">
@@ -90,17 +90,20 @@
                 </div>
 
                    
-                <div class="form-group">   
-                    <input type="submit" name="submit" value="SUBMIT" class="form-control">
+                <div class="form-group row">   
+                    <div class="col-sm-10">
+                    <button type="submit" name="submit" value="SUBMIT" class="btn btn-primary">SUBMIT</button>
+                    </div>
                 </div>
 
                    
                    
                     
             </form>
+            
         </div>
-        <?php include './Public/Template/footer.php';?>
 
+        <?php include './Public/Template/footer.php';?>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
